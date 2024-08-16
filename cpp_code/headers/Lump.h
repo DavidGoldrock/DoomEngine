784df481b2cba56de43b09ac8d7f6b0c205cf7a4
David Goldrock <gaming131213@gmail.com>
2024-08-16 21:26:45 +0300

    seperated lump

--- cpp_code/headers/Lump.h
+++ cpp_code/headers/Lump.h
@@ -11,64 +11,20 @@ struct Lump {
     char* name;
 
     // Constructor
-    Lump(uint16_t filepos, uint16_t size, const char* name)
-    : filepos(filepos), size(size) {
-        // Allocate memory for name and copy the string
-        this->name = new char[std::strlen(name) + 1];
-        std::strcpy(this->name, name);
-    }
+    Lump(uint16_t filepos, uint16_t size, const char* name);
 
-    Lump() {
-        this->filepos = 0;
-        this->size = 0;
-        this->name = nullptr;
-    }
+    Lump();
 
     // Copy constructor
-    Lump(const Lump& other)
-    : filepos(other.filepos), size(other.size) {
-        // Allocate memory and copy the name
-        name = new char[std::strlen(other.name) + 1];
-        std::strcpy(name, other.name);
-    }
+    Lump(const Lump& other);
 
     // Assignment operator
-    Lump& operator=(const Lump& other) {
-        if (this != &other) { // self-assignment check
-            // Free existing resource
-            delete[] name;
-            name = nullptr;
-            // Copy data from other
-            filepos = other.filepos;
-            size = other.size;
-            name = new char[std::strlen(other.name) + 1];
-            std::strcpy(name, other.name);
-        }
-        return *this;
-    }
+    Lump& operator=(const Lump& other);
 
     // Destructor
-    ~Lump() {
-        try
-        {
-            delete[] name;
-        }
-        catch(const std::exception& e)
-        {
-            std::cerr << e.what() << '\n';
-        }
-        
-        name = nullptr;
-    }
+    ~Lump();
 
-    friend std::ostream& operator<<(std::ostream& os, const Lump& obj) {
-        os << "Lump{ ";
-        os << "filepos: " << obj.filepos << " ";
-        os << "size: " << obj.size << " ";
-        os << "name: " << (obj.name ? obj.name : "nullptr") << " ";
-        os << "}";
-        return os;
-    }
+    friend std::ostream& operator<<(std::ostream& os, const Lump& obj);
 };
 
 #endif
