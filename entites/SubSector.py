23b441287b80ffc017876f838f8034a9006d7fc8
David Goldrock <gaming131213@gmail.com>
2024-08-24 19:55:51 +0300

    Merge from cpp

--- entites/SubSector.py
+++
@@ -1,9 +0,0 @@
-class SubSector:
-    def __init__(self, segCount, firstSegNumber):
-        self.segCount = segCount
-        self.firstSegNumber = firstSegNumber
-
-    def __str__(self):
-        return f"{self.segCount=}, {self.firstSegNumber=}"
-
-    __repr__ = __str__
